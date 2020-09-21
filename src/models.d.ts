b32cb3d331ade6049806f0af12a71fe69c90018c
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-09-21 18:15:46 -0400

    Added Recipe model

---
+++ src/models.d.ts
@@ -0,0 +1,11 @@
+interface Recipe {
+    idMeal: string;
+    strMeal: string;
+    strDrinkAlternate: null;
+    strCategory: string;
+    strArea: string;
+    strInstructions: string;
+    strMealThumb: string;
+    strTags: string;
+    strYoutube: string;
+}
