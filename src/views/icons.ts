8e0426eed288dc57240c42e4a5b061a3377ffb35
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-09-22 12:42:30 -0400

    Added some more icons

--- src/views/icons.ts
+++ src/views/icons.ts
@@ -5,3 +5,7 @@ export { default as AboutIcon } from "@material-ui/icons/EmojiObjects";
 export { default as WorkIcon } from "@material-ui/icons/Work";
 export { default as SearchIcon } from "@material-ui/icons/Search";
 export { default as FilterIcon } from "@material-ui/icons/FilterList";
+export { default as VideoIcon } from "@material-ui/icons/OndemandVideo";
+export { default as FlagIcon } from "@material-ui/icons/OutlinedFlag";
+export { default as ChevronRightIcon } from "@material-ui/icons/ChevronRight";
+// export { default as ShareIcon } from "@material-ui/icons/Share";
