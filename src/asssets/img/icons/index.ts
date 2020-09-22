50a3e8a95d55b22e9ffa618bb983f8d593103464
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-09-22 12:38:40 -0400

    Added custom icons for categories

---
+++ src/asssets/img/icons/index.ts
@@ -0,0 +1,29 @@
+import { default as Vegan } from "./vegan.svg";
+import { default as Beef } from "./beef.svg";
+import { default as Breakfast } from "./breakfast.svg";
+import { default as Chicken } from "./chicken.svg";
+import { default as Dessert } from "./dessert.svg";
+import { default as Goat } from "./goat.svg";
+import { default as Lamb } from "./lamb.svg";
+import { default as Pasta } from "./pasta.svg";
+import { default as Pork } from "./pork.svg";
+import { default as Seafood } from "./seafood.svg";
+import { default as Side } from "./side.svg";
+import { default as Starter } from "./starters.svg";
+import { default as Vegetarian } from "./vegetarian.svg";
+
+export const categories = {
+  Vegan,
+  Beef,
+  Breakfast,
+  Chicken,
+  Dessert,
+  Goat,
+  Lamb,
+  Pasta,
+  Pork,
+  Seafood,
+  Side,
+  Starter,
+  Vegetarian,
+};
