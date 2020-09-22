3cd892e0f0b41d0744cb7c03a39c9b9af01a493e
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-09-22 12:39:36 -0400

    Added categories to the model

--- src/models.d.ts
+++ src/models.d.ts
@@ -1,11 +1,26 @@
 interface Recipe {
-    idMeal: string;
-    strMeal: string;
-    strDrinkAlternate: null;
-    strCategory: string;
-    strArea: string;
-    strInstructions: string;
-    strMealThumb: string;
-    strTags: string;
-    strYoutube: string;
+  idMeal: string;
+  strMeal: string;
+  strDrinkAlternate: null;
+  strCategory:
+    | "Vegan"
+    | "Beef"
+    | "Breakfast"
+    | "Chicken"
+    | "Dessert"
+    | "Goat"
+    | "Lamb"
+    | "Pasta"
+    | "Pork"
+    | "Seafood"
+    | "Side"
+    | "Starter"
+    | "Vegetarian";
+  strArea: string;
+  strInstructions: string;
+  strMealThumb: string;
+  strTags: string;
+  strYoutube: string;
+  [key: string]: string;
+}
 }
