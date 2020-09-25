5bef001c287489b3c1964dc5b2078593000cf16f
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-09-25 16:51:19 -0400

    Add get random recipes and search methods to tglobal state and context

--- src/store/Context.ts
+++ src/store/Context.ts
@@ -1,16 +1,23 @@
 import React, { Dispatch, SetStateAction } from "react";
 
 type VoidFunction = () => void;
+
 interface DefaultContext {
+  search: (args?: any) => void;
+  loading: boolean;
   recipeList: RecipeShort[] | null;
-  selectedRecipeId: string | null;
-  setSelectedRecipeId: Dispatch<SetStateAction<string | null>> | VoidFunction;
   setRecipeList: Dispatch<SetStateAction<RecipeShort[] | null>> | VoidFunction;
+  selectedRecipeId: string | null;
+  getRandomRecipes: (arg?: any) => void;
+  setSelectedRecipeId: Dispatch<SetStateAction<string | null>> | VoidFunction;
 }
 
 export const Context = React.createContext<DefaultContext>({
+  search: ([]) => {},
+  loading: false,
   recipeList: null,
-  selectedRecipeId: null,
-  setSelectedRecipeId: () => {},
   setRecipeList: () => {},
+  selectedRecipeId: null,
+  getRandomRecipes: () => {},
+  setSelectedRecipeId: () => {},
 });
--- src/store/useGlobalState.ts
+++ src/store/useGlobalState.ts
@@ -1,13 +1,34 @@
 import { useState } from "react";
+import { makeApiRequest } from "../utils";
 
 export const useGlobalState = () => {
   const [recipeList, setRecipeList] = useState<RecipeShort[] | null>(null);
   const [selectedRecipeId, setSelectedRecipeId] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const search = (keyword?: string) => {
+    makeApiRequest(
+      `https://www.themealdb.com/api/json/v1/1/search.php?s=${keyword}`,
+      (data) => setRecipeList(data.meals),
+      setLoading
+    );
+  };
+
+  const getRandomRecipes = () => {
+    makeApiRequest(
+      `https://www.themealdb.com/api/json/v1/1/random.php`,
+      (data) => setRecipeList(data.meals),
+      setLoading
+    );
+  };
 
   return {
+    search,
+    loading,
     recipeList,
     setRecipeList,
     selectedRecipeId,
+    getRandomRecipes,
     setSelectedRecipeId,
   };
 };
