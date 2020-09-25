b910b82c71ad01b77b59bbfcb959ae87837879d8
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-09-25 19:12:53 -0400

    Calling search method only if input value is truthy else get random recipes

--- src/components/search/SearchInput.tsx
+++ src/components/search/SearchInput.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useEffect } from "react";
 import { SearchIcon, FilterIcon } from "../../views/icons";
 import { debounce } from "../../utils";
 import { useStore } from "../../customHooks";
@@ -9,14 +9,21 @@ interface Props {
 }
 
 export const SearchInput: React.FC<Props> = ({ value, setValue }) => {
-  const { search } = useStore();
+  const { search, getRandomRecipes } = useStore();
 
-  const callApi = useMemo(() => debounce(search, 500), [search]);
+  const callApi = useMemo(() => debounce(search, 800), [search]);
+
+  useEffect(() => {
+    if (Boolean(value.trim())) {
+      return callApi(value);
+    }
+    getRandomRecipes();
+  }, [value, callApi, getRandomRecipes]);
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
-    callApi(e.target.value);
   };
+
   return (
     <>
       <input placeholder="Search" value={value} onChange={(e) => onChange(e)} />
