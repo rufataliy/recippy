76413c9c7d1357137b39fd28869d0d9f5cfabccd
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-09-29 13:37:50 -0400

    Added content wrapper div to the content area to have the same width with filter section

--- src/views/ContentArea.tsx
+++ src/views/ContentArea.tsx
@@ -4,7 +4,9 @@ import "../asssets/styles/content.css";
 export const ContentArea: React.FC = ({ children }) => {
   return (
     <div className="content">
-      <div className="content-inner">{children}</div>
+      <div className="content-wrapper">
+        <div className="content-inner">{children}</div>
+      </div>
     </div>
   );
 };
