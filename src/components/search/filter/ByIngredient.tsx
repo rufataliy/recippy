8f48fc08e9b6e13ad7647a2f007028b6d6065694
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-10-02 22:33:19 -0400

    Added validation for ingredient input

--- src/components/search/filter/ByIngredient.tsx
+++ src/components/search/filter/ByIngredient.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
 import Chip from "@material-ui/core/Chip";
+import Input from "@material-ui/core/Input";
 import { useStore } from "../../../customHooks";
 
 export const ByIngredient = () => {
-  const [ingredients, setIngredients] = useState<string[] | undefined>([]);
+  const [ingredients, setIngredients] = useState<string[] | null>(null);
   const [value, setValue] = useState("");
+  const [error, setError] = useState(false);
   const { searchByIngredients, getRandomRecipes } = useStore();
 
   const removeField = (ingredientToRemove: string) => {
@@ -14,15 +16,37 @@ export const ByIngredient = () => {
         return ingredients.filter(
           (ingredient) => ingredient !== ingredientToRemove
         );
+      } else {
+        return null;
       }
     });
   };
 
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setError(false);
+    setValue(e.target.value);
+  };
+
   const addIngredient = () => {
-    setIngredients((ingredients) => {
-      return ingredients && [...ingredients, value];
-    });
-    setValue("");
+    if (Boolean(value.trim())) {
+      if (ingredients?.includes(value)) {
+        return setValue("");
+      }
+      setIngredients((ingredients) => {
+        if (ingredients) {
+          if (ingredients.includes(value)) {
+            return ingredients;
+          }
+          return [...ingredients, value];
+        } else {
+          return [value];
+        }
+      });
+      setError(false);
+      setValue("");
+    } else {
+      setError(true);
+    }
   };
 
   // TODO: process multiple white spaces to be replaces with one underscore
@@ -37,8 +61,11 @@ export const ByIngredient = () => {
   };
 
   const clearIngredients = () => {
-    setIngredients([]);
-    getRandomRecipes();
+    setIngredients(null);
+    setError(false);
+    if (ingredients) {
+      getRandomRecipes();
+    }
   };
 
   return (
@@ -47,7 +74,8 @@ export const ByIngredient = () => {
         <input
           placeholder="Enter ingredient . . ."
           value={value}
-          onChange={(e) => setValue(e.target.value)}
+          data-error={error}
+          onChange={onChange}
         />
       </div>
       <div className="ingredient-chips">
