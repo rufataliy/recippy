49c819ba84d4566feb7141fafdc3a2362ee67f75
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-10-05 13:41:21 -0400

    Added enter submit functionaity

--- src/components/search/filter/ByName.tsx
+++ src/components/search/filter/ByName.tsx
@@ -1,22 +1,27 @@
-import React, { useMemo, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "@material-ui/core/Button";
-import { debounce } from "../../../utils";
 import { useStore } from "../../../customHooks";
 
 export const ByName: React.FC = () => {
   const { searchByName, getRandomRecipes } = useStore();
   const [value, setValue] = useState("");
-  const callApi = useMemo(() => debounce(searchByName, 800), [searchByName]);
 
   useEffect(() => {
-    if (Boolean(value.trim())) {
-      return callApi(value);
+    if (!Boolean(value.trim())) {
+      getRandomRecipes();
     }
-    getRandomRecipes();
-  }, [value, callApi, getRandomRecipes]);
+  }, [value, getRandomRecipes]);
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === "Enter") {
+      if (Boolean(value)) {
+        return searchByName(value);
+      }
+    }
   };
 
   return (
@@ -24,14 +29,22 @@ export const ByName: React.FC = () => {
       <div className="form-control">
         <input
           placeholder="Enter meal name . . ."
+          onKeyPress={handleKeyDown}
           value={value}
           onChange={onChange}
         />
       </div>
       <div className="btn-group">
-        <Button size="large" className="bg-btn" fullWidth type="submit">
+        <Button
+          onClick={() => searchByName(value)}
+          size="large"
+          className="bg-btn"
+          fullWidth
+          type="submit"
+        >
           search
         </Button>
+        <Button onClick={() => setValue("")}>Clear</Button>
       </div>
     </div>
   );
