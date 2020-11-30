7c14475cb29844b00b14178b25c84eff0065367e
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-11-30 18:55:34 -0500

    Fixed some urls

--- src/pages/api/randomRecipes.tsx
+++ src/pages/api/randomRecipes.tsx
@@ -2,7 +2,7 @@ import { NextApiHandler } from "next";
 
 const handler: NextApiHandler = async (req, res) => {
   const recipes = await fetch(
-    "https://www.themealdb.com/api/json/v2/process.env.API_KEY/randomselection.php"
+    `https://www.themealdb.com/api/json/v2/${process.env.API_KEY}/randomselection.php`
   )
     .then((res) => res.json())
     .then((data) => data);
