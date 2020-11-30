4d37e150442277e5d76d08c91f413b8e82656b97
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-11-30 16:27:43 -0500

    Added api handlers - filter-by

---
+++ src/pages/api/filter-by.tsx
@@ -0,0 +1,22 @@
+import { NextApiHandler } from "next";
+
+const filterBaseUrl = `https://www.themealdb.com/api/json/v2/${process.env.API_KEY}/filter.php`;
+
+const handler: NextApiHandler = async (req, res) => {
+  const { query } = req;
+  console.log(req.query);
+  const formFiltersUrl = (query: { [key: string]: string | string[] }) => {
+    const { filterType, filterValue } = query;
+
+    return `${filterBaseUrl}?${filterType}=${filterValue}`;
+  };
+  const recipes = await fetch(formFiltersUrl(query))
+    .then((res) => res.json())
+    .then((data) => data);
+
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify(recipes));
+};
+
+export default handler;
