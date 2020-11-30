c585d4096b6b221e533eee8e61edf05e786f6a94
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-11-30 16:24:00 -0500

    Added _app and _document

---
+++ src/pages/_app.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from "react";
+import { Layout } from "@/components";
+import "@/asssets/styles/index.css";
+import { StateProvider } from "@/store";
+
+interface Props {
+  Component: React.FunctionComponent;
+  pageProps: any;
+}
+
+const App: React.FC<Props> = ({ Component, pageProps }) => {
+  useEffect(() => {
+    setRootHeight();
+    console.log(window);
+    window.addEventListener("resize", (e) => {
+      if ((e.target as typeof window)?.innerWidth < 500) {
+        setRootHeight();
+      }
+    });
+    function setRootHeight() {
+      const root = document.getElementById("__next");
+      console.log(root);
+      if (root) {
+        root.style.height = window.innerHeight.toString() + "px";
+      }
+    }
+  }, []);
+
+  return (
+    <StateProvider>
+      <Layout>
+        <Component {...pageProps} />
+        <script> </script>
+      </Layout>
+    </StateProvider>
+  );
+};
+
+export default App;
