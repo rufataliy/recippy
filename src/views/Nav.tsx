7cbdff42b93eb73ef26636991ecd754441ad1b99
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-11-30 18:17:02 -0500

    Cenverted links to nexjs one

--- src/views/Nav.tsx
+++ src/views/Nav.tsx
@@ -1,48 +1,56 @@
 import React from "react";
+import Link from "next/link";
 import List from "@material-ui/core/List";
 import Divider from "@material-ui/core/Divider";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import { HomeIcon, AboutIcon, WorkIcon, FavoriteIcon } from "@/views/icons";
-import { Link } from "react-router-dom";
 import "@/asssets/styles/nav.css";
 
 export const Nav: React.FC = () => {
   return (
     <List>
-      <Link to="/">
-        <ListItem button key={"home"}>
-          <ListItemIcon>
-            <HomeIcon />
-          </ListItemIcon>
-          <ListItemText primary={"Home"} />
-        </ListItem>
+      <Link href="/">
+        <a>
+          <ListItem button key={"home"}>
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Home"} />
+          </ListItem>
+        </a>
       </Link>
-      <Link to="/about">
-        <ListItem button key={"about"}>
-          <ListItemIcon>
-            <AboutIcon />
-          </ListItemIcon>
-          <ListItemText primary={"About"} />
-        </ListItem>
+      <Link href="/about">
+        <a>
+          <ListItem button key={"about"}>
+            <ListItemIcon>
+              <AboutIcon />
+            </ListItemIcon>
+            <ListItemText primary={"About"} />
+          </ListItem>
+        </a>
       </Link>
-      <Link to="/credits">
-        <ListItem button key={"credits"}>
-          <ListItemIcon>
-            <FavoriteIcon />
-          </ListItemIcon>
-          <ListItemText primary={"Credits"} />
-        </ListItem>
+      <Link href="/credits">
+        <a>
+          <ListItem button key={"credits"}>
+            <ListItemIcon>
+              <FavoriteIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Credits"} />
+          </ListItem>
+        </a>
       </Link>
       <Divider />
-      <Link to="/hire_me">
-        <ListItem button key={"hireme"}>
-          <ListItemIcon>
-            <WorkIcon />
-          </ListItemIcon>
-          <ListItemText primary={"Hire me"} />
-        </ListItem>
+      <Link href="/hire_me">
+        <a>
+          <ListItem button key={"hireme"}>
+            <ListItemIcon>
+              <WorkIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Hire me"} />
+          </ListItem>
+        </a>
       </Link>
     </List>
   );
