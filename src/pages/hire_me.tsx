7622df05c8885ad6fe49258f8d07495e9c82a0bc
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-11-30 16:47:16 -0500

    Added pages - credits

---
+++ src/pages/hire_me.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+import { ContentArea } from "@/views";
+import { hire_me } from "@/asssets/img";
+import "@/asssets/styles/hireme.css";
+
+const HireMe = () => {
+  return (
+    <ContentArea>
+      <div className="hire-me">
+        <div className="hire-me-img-lg">
+          <img src={hire_me} alt="developer working image - hire-me page" />
+        </div>
+        <div className="hire-me-content">
+          <div>
+            <Typography variant="h4">Hire me</Typography>
+            <div className="hire-me-img-sm">
+              <img src={hire_me} alt="developer working image - hire-me page" />
+            </div>
+            <Typography paragraph>
+              This is a small project of mine inspired by my love to cooking. If
+              you think we could make a great team and build awesome
+              applications check out my other{" "}
+              <Link target="_blank" href="https://rufat.tech/projects">
+                projects
+              </Link>{" "}
+              and{" "}
+              <Link target="_blank" href="https://rufat.tech/contact">
+                contact
+              </Link>{" "}
+              me.
+            </Typography>
+          </div>
+        </div>
+      </div>
+    </ContentArea>
+  );
+};
+
+export default HireMe;
