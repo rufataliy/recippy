9c436ff5ee1afe10fc5824e29cbda2dace6eb25b
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2021-01-10 19:58:41 -0500

    Set up initial testing env

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+const { createJestConfig } = require("@craco/craco");
+const cracoConfig = require("./craco.config");
+const jestConfig = createJestConfig(cracoConfig);
+
+module.exports = jestConfig;
