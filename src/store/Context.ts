0eb043db7e39035e0e5b6f353a5ab4961254e3a7
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2021-01-10 20:01:16 -0500

    Moved default context interface to the models file

--- src/store/Context.ts
+++ src/store/Context.ts
@@ -1,23 +1,5 @@
 import React, { Dispatch, SetStateAction } from "react";
 
-type VoidFunction = () => void;
-
-interface DefaultContext {
-  loading: boolean;
-  recipeList: Recipe[] | null;
-  searchByName: (args?: any) => void;
-  setRecipeList: Dispatch<SetStateAction<Recipe[] | null>> | VoidFunction;
-  reviewLoading: boolean;
-  reviewBarOpen: boolean;
-  getRecipeById: (id: string) => void;
-  reviewedRecipe: Recipe | null;
-  searchByCountry: (arg?: any) => void;
-  resetReviewState: () => void;
-  getRandomRecipes: (arg?: any) => void;
-  searchByIngredients: (args?: any) => void;
-  searchByCategory: (args?: any) => void;
-}
-
 export const Context = React.createContext<DefaultContext>({
   loading: false,
   recipeList: null,
